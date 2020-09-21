0e63c4a3835cc6503975ba0fa0e596e434f5089f
hinata-ysk <hinata.ysk@gmail.com>
2020-09-21 02:19:47 +0000

    Fix design

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -19,3 +19,9 @@ $(document).ready(function () {
 $('#collapseReumeButton').click(function(){
   $(this).hide();
 });
+
+$('div[name=portfolio_modal_thumbnail]>img').click(function(){
+  var target = parseInt($(this).attr('data-slide-to'));
+  // $(this).parents('.modal-body').find('.carousel').carousel(target);
+  $(this).parents('div[name=portfolio_modal_image]').find('div[name=carouselIndicators]').carousel(target);
+});
